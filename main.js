e40551f9fe5fd50343e295cf275b0d523bc46af3
Dawoods7 <dawoodshees@gmail.com>
2022-06-13 19:06:37 +0300

    Switches done

--- main.js
+++ main.js
@@ -171,14 +171,216 @@
 //------------------------------------
 //Function With Arguments
 
-function functionWithArgs(a,b) {
-    console.log(a + b)
-};
+// function functionWithArgs(a,b) {
+//     console.log(a + b)
+// };
 
-functionWithArgs(10, 5)
+// functionWithArgs(10, 5)
 
 
 
 
 //------------------------------------
 // Global Scope
+
+// var myGLobal = 10;
+// //this can be used all around and in any function you want
+
+// function fun1() {
+//     var oopsGlobal = 5 //this can only be used inside this function ONLY!! IF there wasnt a var keyword...this would have been possible to be used outside as well.
+// }
+
+
+
+//------------------------------------
+//Local Scopes and functions
+
+// function myLocalScope(){
+//     myVar = 5;
+// }
+
+// myLocalScope() //This will give output as 5
+
+// console.log(myVar) //this will give an error as myVar is defined Inside a function and cant be used outside of it
+// // Local Variable > global variable .... meaning if local and global variable have same names ...and it is called...local will take the lead
+
+
+
+//------------------------------------
+//Return a function
+// function timesFive(num){
+//     return num * 5;
+// }
+
+// console.log(timesFive(5));
+
+
+
+
+
+//------------------------------------
+// Boolean
+
+// function welcomeToBooleans(){
+//     return false;
+// }
+// console.log(welcomeToBooleans)  //THis will return Boolean 'FALSE'
+
+
+
+
+
+
+
+//------------------------------------
+//IF Statements
+
+// function trueOrFalse(wasThatTrue){
+//     if(wasThatTrue){
+//         return 'Yes, that was true';
+//     }
+//     return 'No, That was false';
+// }
+
+// console.log(trueOrFalse(true)); //if its true..then first return statement will be executed or else the second (if false)
+
+
+
+
+
+//------------------------------------
+//Equality Operators
+
+// function testEqual(val){
+//     if (val == 12){
+//         return 'Equal'
+//     }
+//     return 'Not Equal'
+// }
+
+// console.log(testEqual(10)) // this wil output 'Not equal' because 10 is not equal to 12
+
+// '==' checks if value is same (equality)
+// '===' checks if value and data type is same (strict equality)
+
+
+
+
+
+//------------------------------------
+// InEquality Operators
+
+// function testNotEqual(val){
+//     if (val != 12){
+//         return 'Not Equal'
+//     }
+//     return 'Equal'
+// }
+
+
+// '!=' checks if value is NOt same (Inequality)
+// '!==' checks if value and data type is same (strict Inequality)
+
+
+
+
+//------------------------------------
+// AND operators
+
+// function testLogicalAnd(val) {
+//     if(val > 25 && val < 50){
+//         return 'Yes'
+//     }
+    
+//     return 'No'
+// }
+
+// console.log(testLogicalAnd(30)) //This will output "Yes" as the 'val' is above 25 AND below 50
+
+
+
+
+
+//------------------------------------
+// OR operators
+
+// function testLogicalOr(val) {
+//     if(val < 10 || val > 20){
+//         return 'Outside range'
+//     }
+    
+//     return 'Inside range'
+// }
+
+// console.log(testLogicalAnd(30)) //This will output "Outside Range" as the 'val' either below 10 OR above 20 .
+
+
+
+
+
+
+//------------------------------------
+//Switch statemenets
+
+// function caseInSwitch(val){
+//     var answer = '';
+
+//     switch(val){
+//         case 1:
+//             answer = "alpha"
+//             break;
+//         case 2:
+//             answer = "beta"
+//             break;
+//         case 3:
+//             answer = "gamma"
+//             break
+//         case 4:
+//             answer = "delta"
+//             break    
+//             // the default statement will always return the bottom if the val is not the one specified
+//         default:
+//             answer = "Answer not specified properly"       
+//             break; 
+
+//     }
+//     return answer;
+// }
+
+// console.log(caseInSwitch(1));
+
+
+
+
+
+
+//------------------------------------
+
+//This way mulitple inputs can have same outputs (e.g. 1,2 and 3 will give the outout as 'low')
+// function caseInSwitch(val){
+//     var answer = '';
+
+//     switch(val){
+//         case 1:
+//         case 2:
+//         case 3:
+//             answer = "Low"
+//             break;
+
+//         case 4:
+//         case 5:
+//         case 6: 
+//             answer = "Mid"
+//             break;
+
+//         case 7:
+//         case 8:
+//         case 9: 
+//             answer = "High"
+//             break;
+
+//     }
+//     return answer;
+// }
+
+// console.log(caseInSwitch(1));
